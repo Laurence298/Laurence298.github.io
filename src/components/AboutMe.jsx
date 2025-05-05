ec2063f24a59836bf06e390c3aae3f7b5bf81bf4
Laurence <69992260+bobforzolu@users.noreply.github.com>
2025-05-05 13:27:01 -0600

    git hub pages set up

--- src/components/AboutMe.jsx
+++
@@ -1,9 +0,0 @@
-export default function DescriptionBox({ File }) {
-  return (
-    <section className="">
-      <h1 className="text-2xl font-bold">{File.title}</h1>
-      <img> </img>
-      <p className="my-4">{File.description}</p>
-    </section>
-  );
-}
